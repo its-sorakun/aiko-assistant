467d73c536fd5f19a9b2cb5d648ecfed3684367e
fetchthesauce <rukjaa908@gmail.com>
2026-08-24 23:03:32 +0530

    add c++ file with imports

---
+++ cpu_monitor/cpu_monitor/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <windows.h>
+#include "include/IPlatform.h"
+#include "include/IDeviceManager.h"
+#include "include/ICPUEx.h"
+#include "include/IBIOSEx.h"
+#include "include/IDevice.h"
+
+// returns IPlatform object
+typedef IPlatform& (__stdcall* GetPlatformFunc)();
+
+int main(){
+    std::cout << "Starting CPU Monitor" << std::endl;
+
+    
+    
+
+
+    return 0;
+
+
+}
